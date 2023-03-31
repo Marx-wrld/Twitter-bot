9a5bee7b1700f1d50aa2f0a533195718140b7462
Marx-wrld <iongonga@kabarak.ac.ke>
2023-03-31 13:15:48 +0300

    setting the API keys

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,3 +1,11 @@
-#import tweepy
+import tweepy
 
-print('This is my Twitter bot')
+api_key = "ENTER API KEY"
+api_secret = "ENTER API SECRET KEY"
+
+access_token = "ENTER ACCESS TOKEN"
+access_token_secret = "ENTER ACCESS TOKEN SECRET"
+
+auth = tweepy.OAuthHandler(api_key, api_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
